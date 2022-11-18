ae0c22b7e9ad029fe3d82586a2aec3109b94d805
SparrowBrain <qwxmail@gmail.com>
2022-11-19 00:19:50 +0200

    Added AutoMoqDataAttribute with autofixture to tests

---
+++ BuddySave.TestTools/AutoMoqDataAttribute.cs
@@ -0,0 +1,15 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+
+namespace BuddySave.TestTools
+{
+    public class AutoMoqDataAttribute : AutoDataAttribute
+    {
+        public AutoMoqDataAttribute()
+            : base(() => new Fixture()
+                .Customize(new AutoMoqCustomization()))
+        {
+        }
+    }
+}
