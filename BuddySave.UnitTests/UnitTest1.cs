9f861a40e84fd7213b8f8365fdfdeadc1d020156
SparrowBrain <qwxmail@gmail.com>
2022-11-19 00:09:30 +0200

    Initial interface draft

---
+++ BuddySave.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace BuddySave.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- BuddySave/Program.cs
+++ BuddySave/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace BuddySave
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    internal class GameSave
+    {
+        private string Name { get; set; }
+
+        private string LocalPath { get; set; }
+
+        private string CloudPath { get; set; }
+    }
+
+    internal interface ISharedSaveManager
+    {
+        void Start(GameSave gameSave);
+
+        void Stop(GameSave gameSave);
+    }
+
+    internal interface ICloudManager
+    {
+        void UploadSave(GameSave gameSave);
+
+        void DownloadSave(GameSave gameSave);
+
+        bool LockExists(GameSave save);
+
+        void CreateLock(GameSave save);
+
+        void DeleteLock(GameSave save);
+    }
+}
