ec317dc0eb54143f3bc7bd78598484ab516a1999
SparrowBrain <qwxmail@gmail.com>
2022-11-19 02:20:21 +0200

    CloudManager implementation

---
+++ BuddySave.IntegrationTests/CloudManagerTests.cs
@@ -0,0 +1,287 @@
+using BuddySave.TestTools;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BuddySave.IntegrationTests
+{
+    public class CloudManagerTests
+    {
+        [Theory, AutoMoqData]
+        public async Task UploadSave_DeletesOldCloudSave_WhenCloudSaveExists(
+            string oldSaveFileName,
+            GameSave gameSave,
+            CloudManager sut)
+        {
+            // Arrange
+            using var cloudSave = new TempSaveDir(gameSave.CloudPath);
+            using var localSave = new TempSaveDir(gameSave.LocalPath);
+            gameSave.CloudPath = cloudSave.Path;
+            gameSave.LocalPath = localSave.Path;
+
+            cloudSave.Create();
+            localSave.Create();
+
+            var oldSavePath = Path.Combine(cloudSave.Path, oldSaveFileName);
+            await File.WriteAllTextAsync(oldSavePath, "Test");
+
+            // Act
+            sut.UploadSave(gameSave);
+
+            // Assert
+            Assert.False(Directory.Exists(oldSavePath));
+        }
+
+        [Theory, AutoMoqData]
+        public void UploadSave_CopiesEmptyLocalToCloud_WhenCloudSaveDoesNotExist(
+            GameSave gameSave,
+            CloudManager sut)
+        {
+            // Arrange
+            using var cloudSave = new TempSaveDir(gameSave.CloudPath);
+            using var localSave = new TempSaveDir(gameSave.LocalPath);
+            gameSave.CloudPath = cloudSave.Path;
+            gameSave.LocalPath = localSave.Path;
+
+            localSave.Create();
+
+            // Act
+            sut.UploadSave(gameSave);
+
+            // Assert
+            Assert.True(Directory.Exists(gameSave.CloudPath));
+        }
+
+        [Theory, AutoMoqData]
+        public async Task UploadSave_CopiesFilesToCloud_WhenLocalHasFiles(
+            string file,
+            GameSave gameSave,
+            CloudManager sut)
+        {
+            // Arrange
+            using var cloudSave = new TempSaveDir(gameSave.CloudPath);
+            using var localSave = new TempSaveDir(gameSave.LocalPath);
+            gameSave.CloudPath = cloudSave.Path;
+            gameSave.LocalPath = localSave.Path;
+
+            localSave.Create();
+            var localFilePath = Path.Combine(localSave.Path, file);
+            var cloudFilePath = Path.Combine(cloudSave.Path, file);
+            await File.WriteAllTextAsync(localFilePath, "Test");
+
+            // Act
+            sut.UploadSave(gameSave);
+
+            // Assert
+            Assert.True(File.Exists(cloudFilePath));
+        }
+
+        [Theory, AutoMoqData]
+        public async Task DownloadSave_DeletesOldLocalSave_WhenLocalSaveExists(
+            string oldSaveFileName,
+            GameSave gameSave,
+            CloudManager sut)
+        {
+            // Arrange
+            using var localSave = new TempSaveDir(gameSave.LocalPath);
+            using var cloudSave = new TempSaveDir(gameSave.CloudPath);
+            gameSave.LocalPath = localSave.Path;
+            gameSave.CloudPath = cloudSave.Path;
+
+            localSave.Create();
+            cloudSave.Create();
+
+            var oldSavePath = Path.Combine(localSave.Path, oldSaveFileName);
+            await File.WriteAllTextAsync(oldSavePath, "Test");
+
+            // Act
+            sut.DownloadSave(gameSave);
+
+            // Assert
+            Assert.False(Directory.Exists(oldSavePath));
+        }
+
+        [Theory, AutoMoqData]
+        public void DownloadSave_CopiesEmptyCloudToLocal_WhenLocalSaveDoesNotExist(
+            GameSave gameSave,
+            CloudManager sut)
+        {
+            // Arrange
+            using var localSave = new TempSaveDir(gameSave.LocalPath);
+            using var cloudSave = new TempSaveDir(gameSave.CloudPath);
+            gameSave.LocalPath = localSave.Path;
+            gameSave.CloudPath = cloudSave.Path;
+
+            cloudSave.Create();
+
+            // Act
+            sut.DownloadSave(gameSave);
+
+            // Assert
+            Assert.True(Directory.Exists(gameSave.LocalPath));
+        }
+
+        [Theory, AutoMoqData]
+        public async Task DownloadSave_CopiesFilesToLocal_WhenCloudHasFiles(
+            string file,
+            GameSave gameSave,
+            CloudManager sut)
+        {
+            // Arrange
+            using var localSave = new TempSaveDir(gameSave.LocalPath);
+            using var cloudSave = new TempSaveDir(gameSave.CloudPath);
+            gameSave.LocalPath = localSave.Path;
+            gameSave.CloudPath = cloudSave.Path;
+
+            cloudSave.Create();
+            var cloudFilePath = Path.Combine(cloudSave.Path, file);
+            var localFilePath = Path.Combine(localSave.Path, file);
+            await File.WriteAllTextAsync(cloudFilePath, "Test");
+
+            // Act
+            sut.DownloadSave(gameSave);
+
+            // Assert
+            Assert.True(File.Exists(localFilePath));
+        }
+
+        [Theory, AutoMoqData]
+        public void LockExists_ReturnsFalse_WhenNoLockFile(
+            GameSave gameSave,
+            CloudManager sut)
+        {
+            // Act
+            var result = sut.LockExists(gameSave);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task LockExists_ReturnsTrue_WhenLockFileExists(
+            GameSave gameSave,
+            CloudManager sut)
+        {
+            // Arrange
+            using var lockFile = new TempLockFile();
+            await lockFile.Create();
+            gameSave.CloudPath = lockFile.CloudPath;
+
+            // Act
+            var result = sut.LockExists(gameSave);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task CreateLock_ThrowsException_WhenLockExists(
+            GameSave gameSave,
+            CloudManager sut)
+        {
+            // Arrange
+            using var lockFile = new TempLockFile();
+            await lockFile.Create();
+            gameSave.CloudPath = lockFile.CloudPath;
+
+            // Act
+            var act = new Func<Task>(async () => await sut.CreateLock(gameSave));
+
+            // Assert
+            await Assert.ThrowsAnyAsync<Exception>(act);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task CreateLock_CreatesLock_WhenLockDoesNotExist(
+            GameSave gameSave,
+            CloudManager sut)
+        {
+            // Arrange
+            using var lockFile = new TempLockFile();
+            gameSave.CloudPath = lockFile.CloudPath;
+
+            // Act
+            await sut.CreateLock(gameSave);
+
+            // Assert
+            Assert.True(File.Exists(lockFile.LockPath));
+        }
+
+        [Theory, AutoMoqData]
+        public async Task DeleteLock_DeletesLock_WhenLockExists(
+            GameSave gameSave,
+            CloudManager sut)
+        {
+            // Arrange
+            using var lockFile = new TempLockFile();
+            await lockFile.Create();
+            gameSave.CloudPath = lockFile.CloudPath;
+
+            // Act
+            sut.DeleteLock(gameSave);
+
+            // Assert
+            Assert.False(File.Exists(lockFile.LockPath));
+        }
+
+        [Theory, AutoMoqData]
+        public void DeleteLock_DoesNothing_WhenLockDoesNotExist(
+            GameSave gameSave,
+            CloudManager sut)
+        {
+            // Arrange
+            using var lockFile = new TempLockFile();
+            gameSave.CloudPath = lockFile.CloudPath;
+
+            // Act
+            sut.DeleteLock(gameSave);
+
+            // Assert
+            Assert.False(File.Exists(lockFile.LockPath));
+        }
+
+        private class TempLockFile : IDisposable
+        {
+            public TempLockFile()
+            {
+                CloudPath = Path.GetTempFileName();
+                LockPath = CloudPath + ".lock";
+            }
+
+            public string LockPath { get; }
+
+            public string CloudPath { get; }
+
+            public void Dispose()
+            {
+                File.Delete(LockPath);
+            }
+
+            public async Task Create()
+            {
+                await File.WriteAllTextAsync(LockPath, "This is a test lock");
+            }
+        }
+
+        private class TempSaveDir : IDisposable
+        {
+            public TempSaveDir(string dirName)
+            {
+                Path = System.IO.Path.Combine(System.IO.Path.GetTempPath(), dirName);
+            }
+
+            public string Path { get; }
+
+            public void Dispose()
+            {
+                Directory.Delete(Path, true);
+            }
+
+            public void Create()
+            {
+                Directory.CreateDirectory(Path);
+            }
+        }
+    }
+}
---
+++ BuddySave/CloudManager.cs
@@ -0,0 +1,59 @@
+namespace BuddySave;
+
+public class CloudManager : ICloudManager
+{
+    public void UploadSave(GameSave gameSave)
+    {
+        CopyOverSaves(gameSave.LocalPath, gameSave.CloudPath);
+    }
+
+    public void DownloadSave(GameSave gameSave)
+    {
+        CopyOverSaves(gameSave.CloudPath, gameSave.LocalPath);
+    }
+
+    public bool LockExists(GameSave save)
+    {
+        var lockPath = GetLockPath(save);
+        return File.Exists(lockPath);
+    }
+
+    public async Task CreateLock(GameSave save)
+    {
+        var lockPath = GetLockPath(save);
+        await using var fileWriter = new FileStream(lockPath, FileMode.CreateNew);
+        await using var streamWriter = new StreamWriter(fileWriter);
+        await streamWriter.WriteLineAsync(DateTime.Now.ToString("o"));
+    }
+
+    public void DeleteLock(GameSave save)
+    {
+        var lockPath = GetLockPath(save);
+        File.Delete(lockPath);
+    }
+
+    private static void CopyOverSaves(string sourcePath, string destinationPath)
+    {
+        if (Directory.Exists(destinationPath))
+        {
+            Directory.Delete(destinationPath, true);
+        }
+
+        Directory.CreateDirectory(destinationPath);
+
+        foreach (var file in Directory.GetFiles(sourcePath))
+        {
+            var fileName = Path.GetFileName(file);
+            var destinationFile = Path.Combine(destinationPath, fileName);
+
+            File.Copy(file, destinationFile);
+
+            // TODO: check if file is a folder and copy files with recursion if it is
+        }
+    }
+
+    private static string GetLockPath(GameSave save)
+    {
+        return save.CloudPath + ".lock";
+    }
+}
